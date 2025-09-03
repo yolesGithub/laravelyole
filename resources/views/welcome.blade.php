0b0b79b08f7751602afb924cbe1fc54fefd21477
yolesGitub <haidardaffa27@gmail.com>
2025-09-03 22:35:59 +0700

    Initial commit of Laravel project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>yole network </title>
+</head>
+<body>
+    <h1>Greetings from the Ninja Network</h1>
+    <p>Click the button below to view the list of ninjas.</p>
+
+    <a href="" class="btn">
+        Find Ninjas!
+    </a>
+</body>
+</html>
